22af6f086eaa3f0931a03a417144627e5d57964e
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-11 14:57:26 +0100

    Add app class

---
+++ app.rb
@@ -0,0 +1,23 @@
+require_relative 'classes/author'
+require_relative 'classes/book'
+require_relative 'classes/game'
+require_relative 'classes/genre'
+require_relative 'classes/item'
+require_relative 'classes/label'
+require_relative 'classes/music_album'
+
+
+class App
+    def initialize
+        @items = []
+        @genres = []
+        @labels = []
+        @authors = []
+    end
+
+    def run
+        
+    end
+
+
+end
--- main.rb
+++ main.rb
@@ -2,31 +2,30 @@
 # require_relative 'logic'
 
 def main
-  #   app = App.new
-  #   logic = Logic.new(app)
-
-  puts "Welcome to my Catalog App!\n"
-  option = nil
-  while option != '13'
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all labels'
-    puts '3 - List all Music Albums'
-    puts '4 - List all Genres'
-    puts '5 - List all Games'
-    puts '6 - List all Authors'
-    puts '7 - Create a book'
-    puts '8 - Create a label'
-    puts '9 - Create a Music Album'
-    puts '10 - Create a Genre'
-    puts '11 - Create a Game'
-    puts '12 - Create an Author'
-    puts '13 - Exit'
-    option = gets.chomp
-    # logic.run(option)
-    puts
+    #   app = App.new
+    #   logic = Logic.new(app)
+  
+    puts "Welcome to my Catalog App!\n"
+    option = nil
+    while option != '13'
+      puts 'Please choose an option by entering a number:'
+      puts '1 - List all books'
+      puts '2 - List all music albums'
+      puts '3 - List of games'
+      puts '4 - List all genres'
+      puts '5 - List all labels'
+      puts '6 - List all authors'
+      puts '7 - Add a book'
+      puts '8 - Add a music album'
+      puts '9 - Add a movie'
+      puts '10 - Add a game'
+      puts '11 - Exit'
+      option = gets.chomp
+      # logic.run(option)
+      puts
+    end
+    puts 'Thank you for using this app!'
   end
-  puts 'Thank you for using this app!'
-end
-
-main
+  
+  main
+  
