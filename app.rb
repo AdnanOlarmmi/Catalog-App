cee9b47f077a8517b58fe5bb5b9a75839cee5db8
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-11 15:28:08 +0100

    Create main class

--- app.rb
+++ app.rb
@@ -15,8 +15,33 @@ class App
         @authors = []
     end
 
-    def run
-        
+    def add_book
+        puts "Please enter the title of the book:"
+        title = gets.chomp
+        puts "Please enter the author of the book."
+        author = gets.chomp
+        puts "Please enter the genre of the book."
+        genre = gets.chomp
+        puts "Please enter the publish date of the book"
+        publish_date = gets.chomp
+        puts "Please enter the Publisher of the book"
+        publisher = gets.chomp
+        puts "Please enter the cover state of the book(eg. good, bad)"
+        cover_state = gets.chomp
+        puts "is the book archived?"
+        archived = gets.chomp
+
+        book = Book.new(title, publish_date, publisher, cover_state, archived)
+
+        @items.push(book)
+
+    end
+
+    def list_books
+        books = @items.select { |item| item.is_a? Book }
+        books.each do |book|
+            puts book.name
+        end
     end
 
 
--- main.rb
+++ main.rb
@@ -1,31 +1,31 @@
-# require_relative 'app'
+require_relative 'app'
 # require_relative 'logic'
 
-def main
-    #   app = App.new
-    #   logic = Logic.new(app)
-  
-    puts "Welcome to my Catalog App!\n"
-    option = nil
-    while option != '13'
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all music albums'
-      puts '3 - List of games'
-      puts '4 - List all genres'
-      puts '5 - List all labels'
-      puts '6 - List all authors'
-      puts '7 - Add a book'
-      puts '8 - Add a music album'
-      puts '9 - Add a movie'
-      puts '10 - Add a game'
-      puts '11 - Exit'
-      option = gets.chomp
-      # logic.run(option)
-      puts
+class Main
+
+    def main
+        app = App.new
+        #   logic = Logic.new(app)
+        puts "Welcome to my Catalog App!\n"
+        option = nil
+        while option != '11'
+        puts 'Please choose an option by entering a number:'
+        puts '1 - List all books'
+        puts '2 - List all music albums'
+        puts '3 - List of games'
+        puts '4 - List all genres'
+        puts '5 - List all labels'
+        puts '6 - List all authors'
+        puts '7 - Add a book'
+        puts '8 - Add a music album'
+        puts '9 - Add a movie'
+        puts '10 - Add a game'
+        puts '11 - Exit'
+        option = gets.chomp
+        puts 'Thank you for using this app!'
     end
-    puts 'Thank you for using this app!'
-  end
+
+end
   
   main
   
