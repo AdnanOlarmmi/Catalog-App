78018e55f44caa536507b07d1deeacb9f2b28bda
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-11 10:28:28 +0100

    add item to attr accessor

--- classes/label.rb
+++ classes/label.rb
@@ -1,5 +1,6 @@
 class Label
-    attr_reader :id, :title, :color, :items
+    attr_accessor :items
+    attr_reader :id, :title, :color 
 
     def initialize(title, color)
         @id = Random.rand(1...1000)
