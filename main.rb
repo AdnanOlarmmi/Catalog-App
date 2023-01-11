777fa10b02069a49497d3163396191639ac14339
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-11 15:30:13 +0000

    update

--- main.rb
+++ main.rb
@@ -1,14 +1,22 @@
 require_relative 'app'
+require_relative './modules/book_module'
 # require_relative 'logic'
 
 class Main
+include BookModule
 
-    def main
-        app = App.new
+    def initialize
+       @books = load_book
+    end
+    def save_data
+        create_book
+    end
+    def run
+        # app = App.new
         #   logic = Logic.new(app)
         puts "Welcome to my Catalog App!\n"
-        option = nil
-        while option != '11'
+        # option = nil
+        # while option = '11'
         puts 'Please choose an option by entering a number:'
         puts '1 - List all books'
         puts '2 - List all music albums'
@@ -21,11 +29,31 @@ class Main
         puts '9 - Add a movie'
         puts '10 - Add a game'
         puts '11 - Exit'
-        option = gets.chomp
-        puts 'Thank you for using this app!'
+        option = gets.chomp.to_i
+        input(option)
+        main_options
     end
 
+    def input(option)
+        case option
+        when 1
+            list_books
+        when 7
+            add_book
+        when 11
+            save_data
+            puts 'Thank you'
+        else 
+            puts 'invalid option'
+        end
+    end
 end
   
-  main
+
+def main
+    app = App.new
+    app.run
+  end
+  
+main
   
