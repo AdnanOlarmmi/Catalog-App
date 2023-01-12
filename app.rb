a0997ed58955f38b967d4eb985c62ae1f78a236c
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-12 10:41:11 +0000

    Added methods for add game,list all game and list all authors

--- app.rb
+++ app.rb
@@ -113,4 +113,32 @@ class App
       puts "Name: #{genre.name}"
     end
   end
+  def add_game
+    puts 'Please write multiplayer: '
+    multiplayer = gets.chomp
+  
+    puts 'Please write date of publish [Enter date (yyyy-mm-dd)]'
+    publish_date = gets.chomp
+  
+    puts 'Please write last played date [Enter date (yyyy-mm-dd)]'
+    last_played_date = gets.chomp
+  
+    @games.push(Game.new(multiplayer, publish_date, last_played_date))
+    puts 'Game is created'
+  end
+  
+  def list_all_games
+    puts 'Games:'
+    @games.each do |games|
+      puts "Multiplayer: #{games.multiplayer}, Publish Date: #{games.publish_date},
+      Last played date: #{games.last_played_date}"
+    end
+  end
+  
+  def list_all_authors
+    puts 'There are no authors yet!' if @authors.empty?
+    @authors.each do |author|
+      puts "first name: #{author.first_name}, last name: #{author.last_name}"
+    end
+  end
 end
