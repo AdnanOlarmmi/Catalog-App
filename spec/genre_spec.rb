8aa374061e63e4a7a358c4b79e3e6ee8d2e956fa
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-12 20:03:25 +0100

    Wrote test for genre

---
+++ spec/genre_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../classes/genre'
+
+describe Genre do
+    context 'Testing the genre class'
+    name = 'Action'
+    genre = Genre.new(name)
+    
+    it 'should validate the name of the genre' do
+        expect(genre.name).to eq 'Action'
+    end
+end
