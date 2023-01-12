02064da9db0242f7ef3750ecf151a6c80c6ab918
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-12 18:13:03 +0000

    fix linter error

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../classes/author'
 
 describe Author do
-   context 'Testing the author class'
-   name = 'Ahmed Nazir Musah' 
-   author = Author.new(name)
+  context 'Testing the author class'
+  name = 'Ahmed Nazir Musah'
+  author = Author.new(name)
 
-    it "should validate the first name of the author" do 
-        expect(author.name).to eq 'Ahmed Nazir Musah'
-    end    
+  it 'should validate the first name of the author' do
+    expect(author.name).to eq 'Ahmed Nazir Musah'
+  end
 end
