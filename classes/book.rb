b99de89aedf089af30f882c7e235a799ed9bc195
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-12 20:04:33 +0100

    Fix Linters Erro

--- classes/book.rb
+++ classes/book.rb
@@ -19,4 +19,3 @@ class Book < Item
     end
   end
 end
-
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,32 +1,32 @@
 require_relative '../classes/book'
 
 describe Book do
-    context 'Testing the Book Class'
+  context 'Testing the Book Class'
 
-    title = 'The Lord of the Rings'
-    author = 'J.R.R. Tolkien'
-    publish_date = '1954-07-29'
-    publisher = 'Allen & Unwin'
-    cover_state = 'good'
-    book = Book.new(title, author, publisher, cover_state, publish_date)
+  title = 'The Lord of the Rings'
+  author = 'J.R.R. Tolkien'
+  publish_date = '1954-07-29'
+  publisher = 'Allen & Unwin'
+  cover_state = 'good'
+  book = Book.new(title, author, publisher, cover_state, publish_date)
 
-    it 'should validate the book.title value' do
-        expect(book.title).to eq 'The Lord of the Rings'
-    end
+  it 'should validate the book.title value' do
+    expect(book.title).to eq 'The Lord of the Rings'
+  end
 
-    it 'should validate the book.author value' do
-        expect(book.author).to eq 'J.R.R. Tolkien'
-    end
+  it 'should validate the book.author value' do
+    expect(book.author).to eq 'J.R.R. Tolkien'
+  end
 
-    it 'should validate the book.publish_date value' do
-        expect(book.publish_date).to eq '1954-07-29'
-    end
+  it 'should validate the book.publish_date value' do
+    expect(book.publish_date).to eq '1954-07-29'
+  end
 
-    it 'should validate the book.publisher value' do
-        expect(book.publisher).to eq 'Allen & Unwin'
-    end
+  it 'should validate the book.publisher value' do
+    expect(book.publisher).to eq 'Allen & Unwin'
+  end
 
-    it 'should validate the book.cover_state value' do
-        expect(book.cover_state).to eq 'good'
-    end
+  it 'should validate the book.cover_state value' do
+    expect(book.cover_state).to eq 'good'
+  end
 end
