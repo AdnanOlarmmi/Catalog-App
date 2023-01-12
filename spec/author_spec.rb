3cc9204db9a5b36472df541344848bebaf76ed9c
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-12 18:09:31 +0000

    update author_spec.rb

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,16 +2,10 @@ require_relative '../classes/author'
 
 describe Author do
    context 'Testing the author class'
-   first_name = 'Ahmed' 
-   last_name  = 'Musah'
-   author = Author.new(first_name, last_name)
+   name = 'Ahmed Nazir Musah' 
+   author = Author.new(name)
 
     it "should validate the first name of the author" do 
-        expect(author.first_name).to eq 'Ahmed'
-    end
-
-    it "should validate the second name of the author" do 
-        expect(author.last_name).to eq 'Musah'
-    end
-    
+        expect(author.name).to eq 'Ahmed Nazir Musah'
+    end    
 end
