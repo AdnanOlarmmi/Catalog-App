492405f9ecee4e33c0e3d96cc38e70274836c14f
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-13 09:06:35 +0100

    Create books table

---
+++ data/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    publisher VARCHAR(255) NOT NULL,
+    cover_state VARCHAR(4) NOT NULL,
+    publish_date DATE NOT NULL
+);
+
