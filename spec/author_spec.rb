d761c21567f5f7fd1fc6d5c7d02c3eeaae79efd8
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-13 10:36:23 +0100

    Correct author parameter

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,10 +2,10 @@ require_relative '../classes/author'
 
 describe Author do
   context 'Testing the author class'
-  name = 'Ahmed Nazir Musah'
-  author = Author.new(name)
+  full_name = 'Ahmed Nazir Musah'
+  author = Author.new(full_name)
 
   it 'should validate the first name of the author' do
-    expect(author.name).to eq 'Ahmed Nazir Musah'
+    expect(author.full_name).to eq 'Ahmed Nazir Musah'
   end
 end
